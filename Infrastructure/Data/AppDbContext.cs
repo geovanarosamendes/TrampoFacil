bcaf6663c36ab28495aa348be06cced0fb2087e8
geovana rosa mendes <geovanarosamendes@gmail.com>
2025-04-15 13:35:49 -0300

    feat: Implementação dos relacionamentos

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -1,9 +1,9 @@
 
 using Microsoft.EntityFrameworkCore;
-using Anuncios.Models;
-using Usuarios.Models;
-using Denuncias.Models;
-using InfoProfissionais.Models;
+using Anuncio.Models;
+using Usuario.Models;
+using Denuncia.Models;
+using InfoPro.Models;
 
 namespace TrampoFacil.Infrastructure.Data {
  
@@ -13,10 +13,35 @@ namespace TrampoFacil.Infrastructure.Data {
     public AppDbContext(DbContextOptions<AppDbContext>options) : base(options) { }
     
 
-        public DbSet<Usuario>Usuarios {get; set;}
-        public DbSet<InfoPro>DadosProfissionais {get; set;}
-        public DbSet<Anuncio>Anuncios {get; set;}
-        public DbSet<Denuncia>Denuncias {get; set;}
+        public DbSet<Usuario>Usuario {get; set;}
+        public DbSet<InfoPro>DadosPro{get; set;}
+        public DbSet<Anuncio>Anuncio {get; set;}
+        public DbSet<Denuncia>Denuncia {get; set;}
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        //Usuarios 1:N Anuncios
+        modelBuilder.Entity<Usuario>()
+            .HasMany(u=> u.Anuncios)
+            .WithOne(a => a.Usuario)
+            .HasForeignKey(a => a.UsuarioId);
+
+
+        //Usuarios 1:N Denuncias
+        modelBuilder.Entity<Usuario>()
+            .HasMany(u => u.Denuncias)
+            .WithOne(d => d.Usuario)
+            .HasForeignKey(d => d.UsuarioId);
+
+
+        //Usuarios 1:1 InfoProfissionais
+        modelBuilder.Entity<Usuario>()
+            .HasOne(u => u.InfoProf)
+            .WithOne (ip =>ip.Usuario)
+            .HasForeignKey<InfoPro>(ip => ip.UsuarioId);
+    }
     
 
  }
